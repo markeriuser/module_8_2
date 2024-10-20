295637e2cd75b7c90be7938d959514e700d538cb
Mark Sevi <mr.lms29@mail.ru>
2024-10-20 20:05:11 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,39 @@
+def personal_sum(numbers):
+    result = 0
+    incorrect_data = 0
+
+    for item in numbers:
+        try:
+            result += item
+        except TypeError:
+            print(f'Некорректный тип данных для подсчёта суммы - {item}')
+            incorrect_data += 1
+
+    return result, incorrect_data
+
+
+def calculate_average(numbers):
+    try:
+        if not hasattr(numbers, '__iter__'):
+            raise TypeError
+
+        total, incorrect_data = personal_sum(numbers)
+
+
+        if len(numbers) - incorrect_data == 0:
+            return 0
+
+        return total / (len(numbers) - incorrect_data)
+
+    except ZeroDivisionError:
+        return 0
+    except TypeError:
+        print('В numbers записан некорректный тип данных')
+        return None
+
+
+# Тестирование функции
+print(f'Результат 1: {calculate_average("1, 2, 3")}')
+print(f'Результат 2: {calculate_average([1, "Строка", 3, "Ещё Строка"])}')
+print(f'Результат 3: {calculate_average(567)}')
+print(f'Результат 4: {calculate_average([42, 15, 36, 13])}')
